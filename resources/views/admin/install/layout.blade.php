f4a953f69b3ba5688f8eb17f1fff213e7306330d
rashidul <rashidul69@gmail.com>
2024-12-03 18:31:02 +0600

    Load css/js from cdn on installer page

--- resources/views/admin/install/layout.blade.php
+++ resources/views/admin/install/layout.blade.php
@@ -9,25 +9,10 @@
     <link rel="icon" type="image/png" href="{{river_settings('favicon')}}" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <!-- CSS files -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons@latest/iconfont/tabler-icons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/>
-    <link href="/springcms/admin/assets/css/tabler.min.css" rel="stylesheet"/>
-    <link href="/springcms/admin/assets/css/tabler-flags.min.css" rel="stylesheet"/>
-    <link href="/springcms/admin/assets/css/tabler-payments.min.css" rel="stylesheet"/>
-    <link href="/springcms/admin/assets/css/tabler-vendors.min.css" rel="stylesheet"/>
-    <link rel="stylesheet" type="text/css" href="/springcms/admin/assets/css/toastr.min.css">
-    <link href="/springcms/admin/summernote-0.8.18-dist/summernote-bs5.min.css" rel="stylesheet">
-    <link href="/springcms/admin/assets/css/demo.min.css" rel="stylesheet"/>
-    <link href="/springcms/admin/assets/css/toastr.min.css" rel="stylesheet"/>
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta17/dist/css/tabler.min.css">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-
-
-    {{--for laravel-file-manager--}}
-{{--    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">--}}
-{{--    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">--}}
-{{--    <link rel="stylesheet" href="{{ asset('vendor/file-manager/css/file-manager.css') }}">--}}
 
     @routes
 
@@ -39,18 +24,8 @@
       @yield('content')
 </div>
 
-<!-- Libs JS -->
-<script src="https://code.jquery.com/jquery-3.1.1.min.js" ></script>
-<script src="/springcms/admin/assets/js/toastr.min.js"></script>
-{{-- <script src="/springcms/admin/summernote-0.8.18-dist/summernote-bs5.min.js"></script> --}}
-{{-- <script src="/springcms/admin/dynamic-form.js" defer></script> --}}
-{{-- <script src="https://cdn.tiny.cloud/1/49zw3h254k19bwnkh8tl02reg0pb5t75ndy9nm01w6afbql3/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script> --}}
-
-{{-- <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script> --}}
-<!-- Tabler Core -->
-<script src="/springcms/admin/assets/js/tabler.min.js" defer></script>
-<script src="/springcms/admin/assets/js/demo.min.js" defer></script>
-
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta17/dist/js/tabler.min.js"></script>
 
 @stack('scripts')
 </body>
