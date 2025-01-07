2d20310ef64defa60943d9fdd80a7b699120c70d
Mamunul515 <islammdmamunul515@gmail.com>
2025-01-07 16:35:08 +0600

    faq create prb solve

--- resources/views/admin/faq/index.blade.php
+++ resources/views/admin/faq/index.blade.php
@@ -15,7 +15,7 @@
         </x-slot:breads>
 
         <x-slot:buttons>
-            <a href="{{route('river.faq.create')}}" class="btn btn-primary d-none d-sm-inline-block">
+            <a class="btn btn-primary d-none d-sm-inline-block" id="btn-add-new" href="#">
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                     <line x1="12" y1="5" x2="12" y2="19" />
